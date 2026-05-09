8179ffca29791c18c965b2946e380cc1542d8c5e
mathurapalanimuthu-crypto <mathurapalanimuthu@gmail.com>
2026-05-09 17:20:29 +0530

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#111827",
+        color: "white",
+        fontSize: "2rem",
+      }}
+    >
+      Mathura Portfolio
+    </div>
+  );
+}
